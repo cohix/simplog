daf6ddac7eb6792820460ff5ed06cfbb4d95d798
cohix <connorjhicks@gmail.com>
2018-11-26 13:32:47 -0500

    initial logging implementation

---
+++ logger.go
@@ -0,0 +1,35 @@
+package simplog
+
+import (
+	"fmt"
+
+	flag "github.com/cohix/simplflag"
+)
+
+var includeDebug bool
+
+func init() {
+	_, includeDebug = flag.CheckFlag("debug")
+}
+
+// LogError logs an error
+func LogError(err error) {
+	fmt.Printf("(E) %s\n", err.Error())
+}
+
+// LogWarn logs a warning
+func LogWarn(msg string) {
+	fmt.Printf("(W) %s\n", msg)
+}
+
+// LogInfo logs an information message
+func LogInfo(msg string) {
+	fmt.Printf("(I) %s\n", msg)
+}
+
+// LogDebug logs an information message
+func LogDebug(msg string) {
+	if includeDebug {
+		fmt.Printf("(D) %s\n", msg)
+	}
+}
