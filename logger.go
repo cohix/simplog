5dd25e09d33a45d09754eff45328faa92c7272ec
cohix <connorjhicks@gmail.com>
2018-11-27 15:45:43 -0500

    removed timestamps for now

--- logger.go
+++ logger.go
@@ -13,25 +13,32 @@ func init() {
 	_, includeDebug = flag.CheckFlag("debug")
 }
 
+type logMessage struct {
+	Time     time.Time `json:"time"`
+	Severity string    `json:"severity"`
+	Message  string    `json:"message"`
+	TraceID  string    `json:"trace_id"`
+}
+
 // LogError logs an error
 func LogError(err error) {
-	fmt.Printf("%s (E) %s\n", time.Now(), err.Error())
+	fmt.Printf("(E) %s\n", err.Error())
 }
 
 // LogWarn logs a warning
 func LogWarn(msg string) {
-	fmt.Printf("%s (W) %s\n", time.Now(), msg)
+	fmt.Printf("(W) %s\n", msg)
 }
 
 // LogInfo logs an information message
 func LogInfo(msg string) {
-	fmt.Printf("%s (I) %s\n", time.Now(), msg)
+	fmt.Printf("(I) %s\n", msg)
 }
 
 // LogDebug logs an information message
 func LogDebug(msg string) {
 	if includeDebug {
-		fmt.Printf("%s (D) %s\n", time.Now(), msg)
+		fmt.Printf("(D) %s\n", msg)
 	}
 }
 
